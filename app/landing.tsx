a422ff8bd33c3dd6df421ff0701388a62a1d6411
gyabaahfelix <felixic360@gmail.com>
2025-03-23 11:20:45 +0000

    Initiating authentications into app

--- app/landing.tsx
+++ app/landing.tsx
@@ -1,44 +1,65 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import Colors from '@/data/Colors'
-import Button from '@/components/Shared/Button'
+import { View, Text, Image, Pressable } from "react-native";
+import React from "react";
+import Colors from "@/data/Colors";
+import Button from "@/components/Shared/Button";
+import { useRouter } from "expo-router";
 
 export default function LandingScreen() {
+  const router = useRouter();
   return (
     <View>
-      <Image source={require('./../assets/images/login.png')} 
+      <Image
+        source={require("./../assets/images/login.png")}
         style={{
-          width: '100%',
-          height: 480
+          width: "100%",
+          height: 480,
         }}
       />
-      <View style={{
-        padding: 20,
-      }}>
-        
-        <Text style={{
-          fontSize: 30,
-          fontWeight: 'bold',
-          textAlign: 'center'
-        }}>Welcome to University of Ghana Campus</Text>
+      <View
+        style={{
+          padding: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          Welcome to University of Ghana Campus
+        </Text>
 
-        <Text style={{
-          fontSize: 17,
-          textAlign: 'center',
-          marginTop: 10,
-          color:Colors.GRAY
-        }}>Your College news on your smartphone. Join clubs, register for events and many more!</Text>
+        <Text
+          style={{
+            fontSize: 17,
+            textAlign: "center",
+            marginTop: 10,
+            color: Colors.GRAY,
+          }}
+        >
+          Your College news on your smartphone. Join clubs, register for events
+          and many more!
+        </Text>
 
-        <Button text='Get Started' onPress={()=>console.log('Button Pressed')}/>
+        <Button
+          text="Get Started"
+          onPress={() => router.push("/(auth)/SignUp")}
+        />
 
-        <Text style={{
-          fontSize: 16,
-          textAlign: 'center',
-          color: Colors.GRAY,
-          marginTop: 7,
-        }}>Already have an account? Sign in Here</Text>
-
+        <Pressable onPress={()=>router.push('/(auth)/SignIn')}>
+          <Text
+            style={{
+              fontSize: 16,
+              textAlign: "center",
+              color: Colors.GRAY,
+              marginTop: 7,
+            }}
+          >
+            Already have an account? Sign in Here
+          </Text>
+        </Pressable>
       </View>
     </View>
-  )
+  );
 }
